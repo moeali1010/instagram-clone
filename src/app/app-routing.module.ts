da4ed8a213355702a3f8978e8d039e1ef7c5270e
mohamed1010 <workmail1010@gmail.com>
2021-12-02 14:30:09 +0200

    profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'profile/:id',
+    loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule)
   }
 ];
 @NgModule({
