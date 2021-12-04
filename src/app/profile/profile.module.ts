6485ac2ac0a8041441636a27690febbb1b6a3fe2
mohamed1010 <workmail1010@gmail.com>
2021-12-04 14:04:48 +0200

    chnage user info to reuse comp

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -3,11 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile/profile.component';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { SharedModule } from '../shared/shared.module';
-import { UserInfoComponent } from './user-info/user-info.component';
+
 import { UserPostsComponent } from './user-posts/user-posts.component';
 import { PostDetailComponent } from './post-detail/post-detail.component';
 @NgModule({
-  declarations: [ProfileComponent , UserInfoComponent , UserPostsComponent , PostDetailComponent],
+  declarations: [ProfileComponent  , UserPostsComponent , PostDetailComponent],
   imports: [
     CommonModule,
     ProfileRoutingModule,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -9,10 +9,15 @@ import { GetRandomUsersService } from 'src/app/services/get-random-users.service
 import { ReactiveFormsModule } from '@angular/forms';
 import { ProfileHeaderComponent } from '../reusable-component/profile-header/profile-header.component';
 import { SharedRoutingModule } from './shared-routing.module';
-
+import { UserInfoComponent } from '../reusable-component/user-info/user-info.component';
 @NgModule({
-  declarations: [PostsComponent,  PostCommentComponent , ProfileHeaderComponent],
-  providers:[GetRandomUsersService],
+  declarations: [
+    PostsComponent,
+    PostCommentComponent,
+    ProfileHeaderComponent,
+    UserInfoComponent
+  ],
+  providers: [GetRandomUsersService],
   imports: [
     CommonModule,
     IonicModule,
@@ -20,12 +25,13 @@ import { SharedRoutingModule } from './shared-routing.module';
     ReactiveFormsModule,
     SharedRoutingModule
   ],
-  exports:[
+  exports: [
     IonicModule,
     FormsModule,
     PostsComponent,
-    PostCommentComponent ,
-    ProfileHeaderComponent
+    PostCommentComponent,
+    ProfileHeaderComponent,
+    UserInfoComponent
 
   ]
 })
